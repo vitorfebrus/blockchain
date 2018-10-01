66a8327d965db94228dce86e9ba079a84e89a324
Vitor <Vitor@Eliene-PC>
2018-10-01 18:31:55 -0300

    update

--- blockchain/src/Blockchain.java
+++ blockchain/src/Blockchain.java
@@ -39,7 +39,7 @@ public class Blockchain implements Constants {
 		}
 		// bloco invalido
 		else {
-			System.out.println("Block invalido nao adicionado!");
+			System.out.println("Block invalido nao adicionado! " + blocoNovo);
 			// notificar a rede sobre um no suspeito
 			// pedir o reenvio do block
 			return false;
